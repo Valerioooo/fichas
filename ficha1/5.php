d869c02262086c149458516293665e2fac6c656d
Valerioooh <diogocsvalerio@protonmail.com>
2020-05-20 15:15:56 +0100

    init

---
+++ ficha1/5.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+$a = 17;
+$b = 18;
+$med = ($a+$b+$c)/3
+echo "A média de $a + $b + $c é $med";
+?>
+
+  </body>
+</html>
