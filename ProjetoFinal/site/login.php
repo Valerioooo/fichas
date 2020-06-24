3ded677e054837087d425f636dfb93f86ab1a415
Valerioooo <diogocsvalerio@protonmail.com>
2020-06-24 17:31:05 +0100

    misas

--- ProjetoFinal/site/login.php
+++ ProjetoFinal/site/login.php
@@ -1,12 +1,17 @@
 <?php
+session_start();
+
+
 $ligacao = mysqli_connect('localhost',$_POST['nome'], $_POST['passe']);
 
 mysqli_select_db($ligacao, 'Empresa');
 
 if($ligacao){
-header("location: menu.html");
+	$ses['ligado'] = 1 ;
+	header("location: menu.html");
 
 }else {
+	$ses['ligado'] = 0 ;
 	echo "erro: utilizador ou password incorretos";
 }
 
